73cdde372647101ccb4e84e5f0460bbd77ac6ed5
Grzegorz Wieczorek <grzewster@gmail.com>
2013-07-25 18:10:23 +0200

    init commit

---
+++ Makefile
@@ -0,0 +1,11 @@
+
+obj-m := cdnna.o
+KDIR := /lib/modules/$(shell uname -r)/build
+PWD := $(shell pwd)
+default:
+	$(MAKE) -C $(KDIR) SUBDIRS=$(PWD) modules
+
+clean:
+	$(MAKE) -C $(KDIR) SUBDIRS=$(PWD) clean
+
+
---
+++ cdnna.c
@@ -0,0 +1,34 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/netfilter.h>
+#include <linux/netfilter_ipv4.h>
+
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Grzegorz Wieczorek <grzewster@gmail.com>");
+MODULE_DESCRIPTION("CDN - node alignment");
+MODULE_ALIAS("cdnna");
+
+static struct nf_hook_ops nfhookops;
+
+unsigned int hook_function(unsigned int hooknum, struct sk_buff *skb, const struct net_device *in, const struct net_device *out, int (*okfn)(struct sk_buff *))
+{
+  printk(KERN_INFO "Packet logged\n"); //log to var/log/messages
+  return NF_ACCEPT; //Accept packet
+}
+
+
+int init_module (void) {
+	printk(KERN_INFO "CDNna loaded\n");
+	nfhookops.hook = hook_function;
+	nfhookops.hooknum = NF_INET_PRE_ROUTING;
+	nfhookops.pf = PF_INET; //IPv4
+	nfhookops.priority = NF_IP_PRI_FIRST;
+	nf_register_hook(&nfhookops);
+	return 0;
+}
+
+void cleanup_module(void) {
+	printk(KERN_INFO "CDNna removed\n");
+	nf_unregister_hook(&nfhookops);
+}
